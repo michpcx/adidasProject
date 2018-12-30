59618f8d37695afaf452a6e34274b3b7e0058103
Michal P <43756311+michpcx@users.noreply.github.com>
2018-12-30 13:07:17 +0000

    Fixed problem with scrolling

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
-		var adjustAnchor = function() {
-            var $anchor = $('#scroll-stop'),fixedElementHeight = 55;
-            if ($anchor.length > 0) {
-                $('html, body')
-                    .stop()
-                    .animate({scrollTop: $anchor.offset().top - fixedElementHeight}, 200);
-            }
-        };
         $("#scroll-button").on('click', function() {
             adjustAnchor();
         })
 		document.getElementById("scroll-button").style.cursor = "pointer";
 
 
+
 		$("body").on('click', '.menu_icon', function() {
 			$(".mobile_menu").toggleClass("menu_show");
-		});
+		})
    });
+
+
+
+var adjustAnchor = function() {
+			 var $anchor = $('#scroll-stop'),fixedElementHeight = 55;
+			 if ($anchor.length > 0) {
+					 $('html, body')
+							 .stop()
+							 .animate({scrollTop: $anchor.offset().top - fixedElementHeight}, 200);
+			 }
+	 };
